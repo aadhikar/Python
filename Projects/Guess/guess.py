990fcec99635bcbe162d2612b51f3de17b2bb898
Anilkumar Adhikari <aadhikar@users.noreply.github.com>
2018-03-19 18:24:43 -0500

    updated script
    updated script

--- Projects/Guess/guess.py
+++ Projects/Guess/guess.py
@@ -2,24 +2,68 @@
 
 #import statements
 import random as rand
+import time as time
+
+def getName(userName):
+	return str(userName);
+
+	
+def ordinal(num):
+	numInStr = str(num);
+	ordnl = numInStr[len(numInStr)-1];
+	if ordnl==str(1):
+		ordnl = ordnl+'st';
+	elif ordnl==str(2):
+		ordnl = ordnl+'nd';
+	elif ordnl==str(3):
+		ordnl = ordnl+'rd';
+	else :
+		ordnl = ordnl+'th';
+	return numInStr.replace(numInStr[len(numInStr)-1], ordnl);
+	
+	
+
+def appreciation(name, attemts, flag):
+	congratz = 'Congratulations!!';
+	if attemts==1 and flag:
+		print '{2} Flying colors {0} and you got it in your {1} attempt'.format(name, ordinal(attemts), congratz);
+	elif attemts==2 and flag:
+		print '{2} Not bad {0} and you got it in your {1} attempts'.format(name, ordinal(attemts), congratz);
+	elif attemts==3 and flag:
+		print 'Oooo almost missed it {0} and you got it in your {1} attempts'.format(name, ordinal(attemts));
+	else :
+		print 'You missed it {0} and you took your {1} attempts'.format(name, ordinal(attemts));
+
+def closeApplication():
+	time.sleep(3);
+	print 'Closing application now';
+	time.sleep(2);
 
 def isNumberInRange(number, num_from, num_till):
 	if(num_from<=number>=num_till):
 		print 'You have entered {0}'.format(number);
 		return number;
-	else:
-		print 'You have entered not {0}'.format(number);
+	else :
+		print 'You have entered {0}'.format(number);
+		return number;
 
 def main():
 	#Variables
 	range_from = 1;
 	range_till = 6;
+	isGuessCorrect = False;
 	num = rand.randint(range_from, range_till);
 	guessCount = 0;
-	print(num);
+	#generatedNumber = rand.randint(1, 5);
+	generatedNumber = 3;
+	#print(num);
 	print 'You may get number of chances between 1 and 5. Choose your number carefully';
 
-	while guessCount<rand.randint(1, 5):
+	while guessCount<generatedNumber:
+	  #userName = input('Enter Your Name Here: ');
+	  #userName = getName(input('Enter Your Name Here: '));
+	  #print 'Hello {0}'.format(userName);
+	  userName = 'Test User';
 	  guessedNum = isNumberInRange(int(input('Guess a number between {0} and {1}: '.format(range_from, range_till))), range_from, range_till);
 	  guessCount = guessCount+1;
 	  
@@ -30,13 +74,14 @@ def main():
 		print 'Go lower';
 
 	  if guessedNum == num:
+		isGuessCorrect = True;
+		appreciation(userName, guessCount, isGuessCorrect);
+		closeApplication();
 		break;
 
-	if guessedNum == num:
-	  print 'Correct!! You have guessed number in {0} chances'.format(str(guessCount)) ;
-
 	if guessedNum != num:
-	  print 'NOPE!!!! I was thinking of number {0}'.format(str(num));
+	  appreciation(userName, guessCount, isGuessCorrect);
+	  closeApplication();
   
 
 if __name__ == '__main__':
