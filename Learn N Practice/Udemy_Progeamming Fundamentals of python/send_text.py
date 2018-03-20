90eb3f67d595c36d348aa53c57c17f739a749309
aadhikar <aadhikar@nyit.edu>
2018-03-19 21:18:40 -0500

    masking details
    Masking account and token details

--- Learn N Practice/Udemy_Progeamming Fundamentals of python/send_text.py
+++ Learn N Practice/Udemy_Progeamming Fundamentals of python/send_text.py
@@ -1,7 +1,7 @@
 from twilio.rest import Client
 
-account = "ACd1b5f2a548b2c37e0a92450ce611860e"
-token = "01feea475b99a0ea33366b4d451cb35b"
+account = "ACd1b5f2a548b2c37e0a92450certrtrte"
+token = "01feea475b99a0ea33366brty6567535b"
 client = Client(account, token)
 
 message = client.messages.create(
